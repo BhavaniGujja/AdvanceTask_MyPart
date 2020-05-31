d42eb7c92a8c7f0928accc60391de38eefa43f64
BhavaniGujja <gujjadurgabhavani@gmail.com>
2020-05-31 14:51:32 +1000

    First commit

---
+++ MarsFramework/Pages/SignIn.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using MarsFramework.Global;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MarsFramework.Pages
+{
+   public  class SignIn
+    {
+        public void LoginSteps()
+        {
+            Global.GlobalDefinitions.ExcelLib.PopulateInCollection(Global.Base.ExcelPath, "SignIn");
+
+            //Navigate to the Url
+            Global.GlobalDefinitions.driver.Navigate().GoToUrl(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Url"));
+
+            Global.GlobalDefinitions.driver.FindElement(By.XPath("//a[@class='item']")).Click();
+
+            //using excelhelper class reading data from excel data sheet
+
+
+            Global.GlobalDefinitions.driver.FindElement(By.XPath("//input[@placeholder='Email address']")).SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Username"));
+
+            Global.GlobalDefinitions.driver.FindElement(By.XPath("//input[@placeholder='Password']")).SendKeys(Global.GlobalDefinitions.ExcelLib.ReadData(2, "Password"));
+
+            Global.GlobalDefinitions.driver.FindElement(By.XPath("//button[@class='fluid ui teal button']")).Click();
+        }
+
+        
+    }
+}
---
+++ MarsFramework/Test/Program.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using MarsFramework.Pages;
+using MarsFramework.Global;
+using System;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using Assert = NUnit.Framework.Assert;
+
+namespace MarsFramework
+{
+    [TestClass]
+    public class Program
+    {
+        [TestFixture]
+        [Category("Sprint1")]
+        class User : Global.Base
+        {
+
+            [Test, Order(1)]
+            public void SkillPage()         
+            {
+                test = extent.StartTest("Skills test started");
+                Skills skills = new Skills();
+                skills.AddSkill();
+                skills.UpdateSkill();
+                skills.DeleteSkill();
+
+            }
+
+            [Test, Order(2)]
+            public void EducationPage()
+            {
+                test = extent.StartTest("Education test started");
+                Education education = new Education();
+                education.AddEducation();
+                education.UpdateEducation();
+                education.DeleteEducation();
+            }
+            [Test, Order(3)]
+            public void CertificationPage()
+            {
+                test = extent.StartTest("Certification test started");
+                Certification certification = new Certification();
+                certification.AddCertification();
+                certification.UpdateCertification();
+                certification.DeleteCertification();
+            }
+            [Test, Order(4)]
+            public void Profile()
+            {
+                test = extent.StartTest("Profile Description test started");
+                Profile_Description profile = new Profile_Description();
+                profile.AddDescription();
+            }
+
+
+
+        }
+    }
+}
